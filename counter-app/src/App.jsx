7b5b35ac65e32475fe9c6b83f557ba46aab663a2
poojadayal <poojaadayal@gmail.com>
2025-07-25 15:35:34 +0530

    counter app completed
    Signed-off-by: poojadayal <poojaadayal@gmail.com>

--- counter-app/src/App.jsx
+++ counter-app/src/App.jsx
@@ -1,8 +1,51 @@
+import { useState } from "react";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const [count, setCount] = useState(0);
 
-export default App
+  const increment = () => {
+    if (count < 10) {
+      setCount((prev) => prev + 1);
+    }
+  };
+
+  const decrement = () => {
+    if (count > 0) {
+      setCount((prev) => prev - 1);
+    }
+  };
+
+  const handleReset = () => {
+    setCount(0);
+  };
+
+  return (
+    <div className="border max-w-2xl py-12 my-12 mx-auto">
+      <div className="flex flex-col justify-center items-center">
+        <p className="text-xl font-bold py-4">{count}</p>
+        <div className="flex gap-4">
+          <button
+            className="bg-blue-500 text-white px-4 py-2 rounded"
+            onClick={increment}
+          >
+            Increment +
+          </button>
+          <button
+            className="bg-blue-500 text-white px-4 py-2 rounded"
+            onClick={handleReset}
+          >
+            Reset
+          </button>
+          <button
+            className="bg-blue-500 text-white px-4 py-2 rounded"
+            onClick={decrement}
+          >
+            Decrement -
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
